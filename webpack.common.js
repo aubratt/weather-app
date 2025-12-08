33f1d0df59580874f9e761cff11382a2b6075853
Austin <ausbratton@gmail.com>
2025-12-08 15:06:06 -0600

    Move devServer to webpack.dev

--- webpack.common.js
+++ webpack.common.js
@@ -15,17 +15,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"), // Serve from dist
-    },
-    compress: true,
-    port: 3000,
-    hot: true, // Enable Hot Module Replacement
-    liveReload: true, // Reload page when files change
-    watchFiles: ["src/**/*", "public/**/*"], // Watch these files for changes
-    open: true, // Open browser automatically
-  },
   module: {
     rules: [
       {
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,14 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    static: "./dist",
+    static: {
+      directory: path.join(__dirname, "dist"), // Serve from dist
+    },
+    compress: true,
+    port: 3000,
+    hot: true, // Enable Hot Module Replacement
+    liveReload: true, // Reload page when files change
+    watchFiles: ["src/**/*", "public/**/*"], // Watch these files for changes
+    open: true, // Open browser automatically
   },
 });
